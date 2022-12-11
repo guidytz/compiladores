7db5c456c23e5844a601a9edf148a03771f6f1a0
guidytz <guidytz@gmail.com>
2022-12-11 14:54:04 -0300

    Criando testes base

---
+++ etapa1/tests/create.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+create=$1
+
+if [[ $create == "specials" ]]; then
+    specials=", ; : ( ) { } + - / % ^ < > = !" # falta o '*'
+    count=0
+    for ch in $specials; do
+        ((++count))
+        echo "Creating: $ch"
+        echo -n $ch > "inputs/sp${count}.txt"
+        echo -n "1 TK_ESPECIAL [$ch]" > "outputs/sp${count}.txt"
+    done
+fi
+
+if [[ $create == "int" ]]; then
+    lit_int="1 2 10 -5 0123 045 0098 -15"
+    count=0
+    for ch in $lit_int; do
+        echo "Creating: $ch"
+        ((++count))
+        echo -n $ch > "inputs/lit_int${count}.txt"
+        echo -n "1 TK_LIT_INT [$ch]" > "outputs/lit_int${count}.txt"
+    done
+fi
+
+if [[ $create == "float" ]]; then
+    lit_float="1.09 2.123 10.0 -5.76 0123.098 045.123 0098.1230 -15.76 12.0e-2 3.0e1 12.2E4 -09.2E9"
+    count=0
+    for ch in $lit_float; do
+        echo "Creating: $ch"
+        ((++count))
+        echo -n $ch > "inputs/lit_float${count}.txt"
+        echo -n "1 TK_LIT_FLOAT [$ch]" > "outputs/lit_float${count}.txt"
+    done
+fi
+
+if [[ $create == "char" ]]; then
+    lit_char="'1' 'a' 'b' ',' ';' ':' '(' ')' '{' '}' '+' '-' '/' '%' '^' '<' '>' '=' '!' '*' ''" # falta o ' '
+    count=0
+    for ch in $lit_char; do
+        echo "Creating: $ch"
+        ((++count))
+        echo -n $ch > "inputs/lit_char${count}.txt"
+        echo -n "1 TK_LIT_CHAR [$ch]" > "outputs/lit_char${count}.txt"
+    done
+fi
+
--- etapa1/tests/test.sh
+++ etapa1/tests/test.sh
@@ -1,11 +1,66 @@
 #!/bin/bash
 
-inputs=$(ls inputs)
-outputs=$(ls outputs)
-for i in "${!inputs[@]}"; do
-    echo -n "Input: "
-    cat "inputs/${inputs[i]}"
-    echo -n " Expected Output: "
-    cat "outputs/${outputs[i]}"
-    echo
+SUCCESS=0
+FAIL=1
+
+failedTests=0
+passedTests=0
+
+runTestScript () {
+    script=$1
+    expectedOutput=$2
+
+    ((inputCounter++))
+    echo "Test $inputCounter"
+
+    expect -c "$script"
+    result=$?
+
+    if [ $result -eq $FAIL ]
+    then
+        echo "TEST FAILED!"
+        echo "Expected output: $expectedOutput"
+        ((failedTests++))
+    else
+        echo "SUCCESS!"
+        ((passedTests++))
+    fi
+
+    echo ""
+}
+
+inputs=(inputs/*)
+outputs=(outputs/*)
+for i in ${!inputs[@]}; do
+    input=$(cat "${inputs[i]}")
+    input="${input//"["/"\\["}"
+    input="${input//"]"/"\\]"}"
+    input="${input//"\""/"\\\""}"
+
+    output=$(cat "${outputs[i]}")
+    output="${output//"["/"\\["}"
+    output="${output//"]"/"\\]"}"
+    output="${output//"\""/"\\\""}"
+
+    script='
+        set timeout 1
+
+        set input "'$input'"
+        set output "'$output'"
+
+        spawn -noecho "../etapa1"
+        send_user "Input: "
+        send -- "$input\n"
+
+        expect {
+            -ex "$output" { exit '"$SUCCESS"' }
+            default { exit '"$FAIL"' }
+        }
+
+        exit 1
+    '
+
+    runTestScript "$script" "$output"
 done
+echo "Failed: $failedTests"
+echo "Passed: $passedTests"
