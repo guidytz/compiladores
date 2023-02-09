3ab936cd4dc3ff59cf0d9abeeb7b37f495172920
Guilherme Dytz <guidytz@gmail.com>
2023-02-08 22:54:12 -0300

    E3: aux python graph gen file from ast output

---
+++ etapa3/gen_graph.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def main(args):
+    file_name = args[1]
+    with open(f"{file_name}") as file:
+        lines = file.readlines()
+        lines = [line.replace("0x", "n") for line in lines]
+        labels = [line[:-1] for line in lines if line.find("label") != -1]
+        nodes = [line[:-1].replace(",", " ->") + ";" for line in lines if line.find("label") == -1]
+
+        print("digraph G {")
+        for label in labels:
+            print(f"    {label}")
+        for node in nodes:
+            print(f"    {node}")
+        print("}")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
