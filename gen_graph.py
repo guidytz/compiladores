4d4db379275458ebeaefcb7765e191557da19d5a
Guilherme Dytz <guidytz@gmail.com>
2023-02-26 20:06:26 -0300

    Moving script that builds dot trees

--- gen_graph.py
+++ gen_graph.py
@@ -5,7 +5,7 @@ def main(args):
     file_name = args[1]
     with open(f"{file_name}") as file:
         lines = file.readlines()
-        lines = [line.replace("0x", "n") for line in lines]
+        lines = [line.replace("0x", "x") for line in lines]
         labels = [line[:-1] for line in lines if line.find("label") != -1]
         nodes = [line[:-1].replace(",", " ->") + ";" for line in lines if line.find("label") == -1]
 
